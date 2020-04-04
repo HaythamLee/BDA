e5f0f4f0d8b96780c91467da4e9eed74e0eadc9e
OscarC <oscarchen1998531@gmail.com>
2020-04-05 00:04:32 +0800

    add

---
+++ code/missingvalues.R
@@ -0,0 +1,11 @@
+had <- read.csv("data/HAD.csv")
+str(had)
+summary(had)
+
+# install.packages("mice")
+library(mice)
+# install.packages("VIM")
+library(VIM) 
+hw1.aggrplot<-aggr(had, col=c('lightblue','red'), 
+                   numbers=TRUE, prop = TRUE, sortVars=TRUE, 
+                   labels=names(had), cex.axis=.7, gap=3)
