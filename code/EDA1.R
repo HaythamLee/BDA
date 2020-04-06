0aba862567ec218762ef89c6bbc4e41853bacda3
HaythamLee <arutearisu@gmail.com>
2020-04-06 22:23:48 +0800

    rm "B" from data

--- code/EDA1.R
+++ code/EDA1.R
@@ -2,7 +2,7 @@
 pkg <- c("dplyr", "ggplot2", "scales")
 lapply(pkg, library, character.only=T)
 
-had <- read.csv("data/HAD.csv")
+had <- read.csv("data/HAD.csv",na.strings = "B")
 # str(had)
 summary(had)
 attach(had)
@@ -14,6 +14,7 @@ pie <- function(x){
     # arrange(desc(x)) %>%
     mutate(prop = Freq / sum(d$Freq) *100)
   print(d)
+  
   ggplot(d, aes(x="", y=Freq, fill=x)) +
     geom_bar(stat="identity", width=1, color="white") +
     coord_polar("y", start=0) +
@@ -30,4 +31,6 @@ pie(FMTREGION)
 pie(FMTBURDEN)
 
 ##=========================================================
-
+ggplot(had, aes(x=VALUE)) + 
+  geom_histogram(aes(y=..density..), colour="black", fill="white")+
+  geom_density(alpha=.2, fill="#FF6666") 
